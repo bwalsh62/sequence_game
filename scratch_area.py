8e11f6e946075000861ba70b9b034858efbb814e
Ben Walsh <bwalsh62@gmail.com>
2020-01-20 22:40:20 -0600

    Adding buttons at each space

--- scratch_area.py
+++ scratch_area.py
@@ -12,8 +12,8 @@ https://en.wikipedia.org/wiki/Sequence_(game)
 #%% Import libraries
 
 # GUI with tkinter
-from tkinter import Tk, Frame, Label, RAISED
-# Button, Scale, Frame, Entry, PhotoImage, ttk, OptionMenu, StringVar
+from tkinter import Tk, Frame, Label, RAISED, Button
+# , Scale, Frame, Entry, PhotoImage, ttk, OptionMenu, StringVar
 
 #%% Generate deck
 
@@ -30,10 +30,10 @@ deck = cards_hearts
 #%% Layout GUI
 root = Tk()
 root.title('sequence')
-root.geometry('{}x{}'.format(400, 400))
+root.geometry('{}x{}'.format(400, 500))
 
 # Grid size
-(rows,cols) = (5,5)
+(rows,cols) = (6,7)
 
 # Card size
 card_width = 50
@@ -43,9 +43,11 @@ card_height = 70
 #------------
 frame_list = [Frame(root, bg='white', width=card_width, height=card_height, pady=2,relief=RAISED) for idx in range(rows*cols)]
 lbl_list = [Label(frame_list[idx], text=card, font=("Arial", 12),bg='White') for idx,card in enumerate(deck[:(rows*cols)])]
+btn_list = [Button(frame_list[idx], text='Place', font=("Arial", 12),bg='White') for idx in range(rows*cols)]
 for row in range(rows):
     for col in range(cols):
         frame_list[row*rows+col].grid(row=row, column=col)
         lbl_list[row*rows+col].grid(row=row, column=col)
+        btn_list[row*rows+col].grid(row=row+1, column=col)
 
 root.mainloop()
